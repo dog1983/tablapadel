3b947cb0613c3f8643a962846dbf955ce8aa4695
dog1983 <damiangomez@Air-de-Damian.fibertel.com.ar>
2023-02-26 12:46:02 -0300

    cvprueba

---
+++ function.js
@@ -0,0 +1,27 @@
+document.getElementById('botonverde').onclick = function() {
+    document.getElementById("cabecera").style.backgroundColor = "rgb(139, 222, 139)"
+}
+document.getElementById('botonrojo').onclick = function() {
+    document.getElementById("cabecera").style.backgroundColor = "rgb(211, 97, 171)"
+}
+document.getElementById('botonazul').onclick = function() {
+    document.getElementById("cabecera").style.backgroundColor = "rgb(41, 147, 218)"
+}
+document.getElementById('botonverdebl').onclick = function() {
+    document.getElementById("barralateral").style.backgroundColor = "rgb(139, 222, 139)"
+}
+document.getElementById('botonrojobl').onclick = function() {
+    document.getElementById("barralateral").style.backgroundColor = "rgb(211, 97, 171)"
+}
+document.getElementById('botonazulbl').onclick = function() {
+    document.getElementById("barralateral").style.backgroundColor = "rgb(41, 147, 218)"
+}
+document.getElementById('botonverdecl').onclick = function() {
+    document.getElementById("columnaprincipal").style.backgroundColor = "rgb(139, 222, 139)"
+}
+document.getElementById('botonrojocl').onclick = function() {
+    document.getElementById("columnaprincipal").style.backgroundColor = "rgb(211, 97, 171)"
+}
+document.getElementById('botonazulcl').onclick = function() {
+    document.getElementById("columnaprincipal").style.backgroundColor = "rgb(41, 147, 218)"
+}
